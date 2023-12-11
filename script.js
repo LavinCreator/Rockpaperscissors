75ef1c2e9600e3e51fa6dbd79077cd8f6308b929
LavinCreator <chanduvls25@gmail.com>
2023-12-11 20:15:19 +0530

    made some changes

--- script.js
+++ script.js
@@ -1,58 +1,58 @@
-function getComputerChoice (){
+let playerScore = 0;
+let computerScore = 0;
+const resultDiv = document.getElementById("result");
+
+// Add event listeners to buttons
+const buttons = document.querySelectorAll(".selection");
+buttons.forEach(button => {
+    button.addEventListener("click", function(event) {
+        playRound(event.target.id); // Get the ID of the clicked button as playerSelection
+    });
+});
+
+function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
-function Round(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    const choices = ['rock', 'paper', 'scissors'];
-    if (!choices.includes(playerSelection)) {
-      return 'Invalid input. Please choose Rock, Paper, or Scissors.';
-    }
-    if (playerSelection === computerSelection) {
-      return 'It\'s a tie!';
-    } else if (
-      (playerSelection === 'rock' && computerSelection === 'scissors') ||
-      (playerSelection === 'scissors' && computerSelection === 'paper') ||
-      (playerSelection === 'paper' && computerSelection === 'rock')
-    ) {
-      return `You Win! ${playerSelection} beats ${computerSelection}`;
-    } else {
-      return `You Lose! ${computerSelection} beats ${playerSelection}`;
-    }
-  }
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-  for (let round = 1; round <= 5; round++) {
-    const playerSelection = getPlayerSelection();
+
+function playRound(playerSelection) {
     const computerSelection = getComputerChoice();
-    const roundResult = Round(playerSelection, computerSelection);
-    console.log(`Round ${round}: ${roundResult}`);
-    if (roundResult.includes('Win')) {
-      playerScore++;
-    } else if (roundResult.includes('Lose')) {
-      computerScore++;
+    
+    // Display player and computer selections
+    resultDiv.innerHTML = `Player selected: ${playerSelection}. Computer selected: ${computerSelection}.<br>`;
+    
+    if (playerSelection === computerSelection) {
+        resultDiv.innerHTML += 'It\'s a tie!';
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper') ||
+        (playerSelection === 'paper' && computerSelection === 'rock')
+    ) {
+        resultDiv.innerHTML += `You Win! ${playerSelection} beats ${computerSelection}`;
+        playerScore++;
+    } else {
+        resultDiv.innerHTML += `You Lose! ${computerSelection} beats ${playerSelection}`;
+        computerScore++;
     }
-  }
-  console.log('Game Over');
-  console.log(`Player Score: ${playerScore}`);
-  console.log(`Computer Score: ${computerScore}`);
-  if (playerScore > computerScore) {
-    console.log('You win the game!');
-  } else if (playerScore < computerScore) {
-    console.log('Computer wins the game.');
-  } else {
-    console.log('It\'s a tie game.');
-  }
-}
-function getPlayerSelection() {
-  let input;
-  do {
-    input = prompt('Enter your choice: Rock, Paper, or Scissors').toLowerCase();
-  } while (input !== 'rock' && input !== 'paper' && input !== 'scissors');
-  return input;
+
+    // Update scores and check for winner
+    updateScores();
 }
 
-// Start the game
-game();
+function updateScores() {
+    resultDiv.innerHTML += `<br>Player Score: ${playerScore} Computer Score: ${computerScore}<br>`;
+    if (playerScore >= 5 || computerScore >= 5) {
+        if (playerScore > computerScore) {
+            resultDiv.innerHTML += 'You win the game!';
+        } else if (playerScore < computerScore) {
+            resultDiv.innerHTML += 'Computer wins the game.';
+        } else {
+            resultDiv.innerHTML += 'It\'s a tie game.';
+        }
+        // Disable buttons after game over
+        buttons.forEach(button => {
+            button.disabled = true;
+        });
+    }
+}
